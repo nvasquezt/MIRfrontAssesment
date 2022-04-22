8f948f46e2b92ab9878f366eae1a11a939d5ad89
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-04-22 02:25:06 -0500

    hotfix: bugs on timer fixed

--- src/Components/ProductCard/index.jsx
+++ src/Components/ProductCard/index.jsx
@@ -1,4 +1,3 @@
-import './ProductCard.scss';
 import { getAProductThunk } from '../../Store/actions';
 import { Link } from 'react-router-dom';
 import { PRODUCT_DETAIL_ROUTE } from 'src/Constants/Router';
@@ -6,28 +5,23 @@ import { useDispatch } from 'react-redux';
 import Button from '../Button';
 import Countdown from 'react-countdown';
 import PropTypes from 'prop-types';
-import React, {useState, useEffect} from 'react';
-
+import React, {useState } from 'react';
+import './ProductCard.scss';
 
 const ProductCard = (props) => {
   const { id,image, title } = props;
-  const [random, setRandom] = useState(0);
   const [changeClass, setChangeClass] = useState(true);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    setRandom(Math.floor(Math.random() * 5) + 1);
-  }, [])
 
   const handleClick = async () => {
     dispatch(getAProductThunk(id));
   }
   const handleComplete = () => {
     setChangeClass(false);
-    setRandom(0);
   }
 
   return (
+
     <div className="productCard">
       <div className="productCard__containerImage">
         <img  className='productCard__containerImage--image' src={image} alt={title} />
@@ -35,9 +29,8 @@ const ProductCard = (props) => {
       <div className="productCard__title">
         {title}
       </div>
-
       <div className={changeClass ? 'productCard__button' : 'productCard__button--disabled'}>
-        <Countdown date={Date.now() + (random * 40000)} onComplete={handleComplete} />
+        <Countdown date={Date.now()+10000 * (Math.floor(Math.random() * 5) + 1)} onComplete={handleComplete} />
         <Link to={`${PRODUCT_DETAIL_ROUTE}/${id}`}>
           <Button name="Go to Details" type="button" handleClick={handleClick} />
         </Link>
